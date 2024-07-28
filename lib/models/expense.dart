38d2b2b0728089063efb791cbc9ede598b82394e
zeynep-sena <zsyurdadur@gmail.com>
2024-07-28 16:20:24 +0300

    added chart, end of module

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -9,19 +9,19 @@ final formatter = DateFormat.yMd();
 enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
-  Category.food : Icons.lunch_dining,
-  Category.travel : Icons.flight_takeoff,
-  Category.leisure : Icons.movie,
-  Category.work : Icons.work,
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
 };
 
 class Expense {
   Expense({
     required this.title,
-    required this.amount, 
-    required this.date, 
+    required this.amount,
+    required this.date,
     required this.category,
-  }) : id = uuid.v4() ;
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
@@ -31,5 +31,30 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
